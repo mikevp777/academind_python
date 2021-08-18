bd7428cfcb7cc900b04b99e59d55be1265435f91
mikevp777 <mikevp777@gmail.com>
2021-08-18 21:15:17 +0300

    1st commit

---
+++ task01.py
@@ -0,0 +1,11 @@
+name = 'Michael'
+age = 49
+
+
+def print_name_age():
+    name_and_age = 'Your name is '+name+'and your age is '+age
+    print(name_and_age)
+
+    
+def print_two_values(val1, val2):
+    print(String(val1))
