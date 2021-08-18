7e445de30b063b83cc2c07069eb68c1faec53c16
mikevp777 <mikevp777@gmail.com>
2021-08-18 21:24:45 +0300

    1st version

--- task01.py
+++ task01.py
@@ -3,9 +3,22 @@ age = 49
 
 
 def print_name_age():
-    name_and_age = 'Your name is '+name+'and your age is '+age
+    name_and_age = 'Your name is '+name+'and your age is '+str(age)
     print(name_and_age)
 
-    
-def print_two_values(val1, val2):
-    print(String(val1))
+
+def print_two(val1, val2):
+    """
+    Gets two numbers and prints them
+    """
+    print(val1)
+    print(val2)
+
+
+def calculate_decades(val_age):
+    return val_age//10
+
+
+print_name_age()
+print_two(25, [1, 2, 3, 'text'])
+print('You lived for '+str(calculate_decades(age))+' decades')
